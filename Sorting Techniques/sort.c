dd488fec66847be00aa83b384f0db05467266872
GunGupta0601 <gungupta0601@gmail.com>
2026-05-29 21:47:04 +0530

    Merge Sort (Iterative and Recursive)

--- Sorting Techniques/sort.c
+++ Sorting Techniques/sort.c
@@ -93,15 +93,65 @@ void quickSort(int A[], int l, int h)
   }
 }
 
+void merge(int A[], int l, int mid, int h)
+{
+  int i = l, j = mid + 1, k = l;
+  int B[100];
+  while (i <= mid && j <= h)
+  {
+    if (A[i] < A[j])
+      B[k++] = A[i++];
+    else
+      B[k++] = A[j++];
+  }
+  for (; i <= mid; i++)
+    B[k++] = A[i];
+  for (; j <= h; j++)
+    B[k++] = A[j];
+  for (i = l; i <= h; i++)
+    A[i] = B[i];
+}
+
+void ImergeSort(int A[], int n)
+{
+  int p, l, h, mid, i;
+  for (p = 2; p <= n; p = p * 2)
+  {
+    for (i = 0; i + p - 1 < n; i = i + p)
+    {
+      l = i;
+      h = i + p - 1;
+      mid = (l + h) / 2;
+      merge(A, l, mid, h);
+    }
+  }
+  if (p / 2 < n)
+    merge(A, 0, p / 2 - 1, n - 1);
+}
+
+void mergeSort(int A[], int l, int h)
+{
+  int mid;
+  if (l < h)
+  {
+    mid = (l + h) / 2;
+    mergeSort(A, l, mid);
+    mergeSort(A, mid + 1, h);
+    merge(A, l, mid, h);
+  }
+}
+
 int main()
 {
-  // int A[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2}, n = 10, i;
+  int A[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2}, n = 10, i;
   // int A[] = {3, 7, 9, 10}, n = 4, i;
-  int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3, __INT16_MAX__}, n = 11, i;
+  // int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3, __INT16_MAX__}, n = 11, i;
   // BubbleSort(A, n);
   // InsertionSort(A, n);
   // SelectionSort(A, n);
-  quickSort(A, 0, n);
+  // quickSort(A, 0, n - 1);
+  // ImergeSort(A, n);
+  mergeSort(A, 0, n - 1);
   for (i = 0; i < n; i++)
   {
     printf("%d ", A[i]);
